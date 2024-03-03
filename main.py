d8041d421c912b1156de1ad677490c8c951dc90d
lezagek <elizaveta.chemeriskaya@gmail.com>
2024-03-03 17:08:10 +1000

    Добавила небольшой функционал для окна "Редактор базы знаний".
    При нажатии на кнопки прошлые виджеты удаляются, а нужные появляются.
    
    Новые виджеты появляются только при нажатии на кнопки "Класс почвы" и "Признаки".
    
    Кнопка "Назад" закрывает окно.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class EditorDB(tk.Toplevel):
     def __init__(self):
         super().__init__(root)
         self.init_editorDB()
-
+    
     def init_editorDB(self):
         self.title('Редактор базы знаний')
         self.geometry('650x450+350+250')
@@ -41,39 +41,158 @@ class EditorDB(tk.Toplevel):
         self.grid_rowconfigure(0, weight=0)
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=2)
 
-        back_frame = tk.Frame(self, bg='grey', bd=10)
+        back_frame = tk.Frame(self, bd=10)
         back_frame.grid(row=0, column=0, columnspan=2, sticky='we')
 
         btn_frame = tk.Frame(self, bg='#D9D9D9', bd=10)
         btn_frame.grid(row=1, column=0)
+        # btn_frame.grid_propagate(1)
 
-        fld_frame = tk.Frame(self, bg='#D9D9D9', bd=110)
-        fld_frame.grid(row=1, column=1)
+        fld_frame = tk.Frame(self, bg='#D9D9D9', bd=10)
+        fld_frame.grid(row=1, column=1, sticky='wesn', padx=30, pady=30)
 
-        btn_back = tk.Button(back_frame, text='Назад')
+        btn_back = tk.Button(back_frame, text='Назад', command=lambda: self.destroy())
         btn_back.grid()
+
+
+        # Все кнопки приводим в нормальное состояние
+        def normal_btn():
+            btn_class_soil['state'] = 'normal'
+            btn_sign['state'] = 'normal'
+            btn_possible_values['state'] = 'normal'
+            btn_class_sign['state'] = 'normal'
+            btn_class_values['state'] = 'normal'
+            btn_check['state'] = 'normal'
+
         
-        btn_class_soil = tk.Button(btn_frame, text='Класс почвы')
+        # Удаляем виджеты той кнопки, которая была нажата
+        def delete_btn():
+            if btn_class_soil['state'] == 'disabled':
+                del_class_solid()
+            elif btn_sign['state'] == 'disabled':
+                del_sign()
+            elif btn_possible_values['state'] == 'disabled':
+                del_possible_values()
+            elif btn_class_sign['state'] == 'disabled':
+                del_class_sign()
+            elif btn_class_values['state'] == 'disabled':
+                del_class_values()
+            elif btn_check['state'] == 'disabled':
+                del_check()
+
+        # Удаляются все виджеты для работы с классом
+        def del_class_solid():
+            class_name.grid_forget()
+            class_name_entry.grid_forget()
+            class_name_add.grid_forget()
+            class_list.grid_forget()
+        
+        # Удаляются все виджеты для работы с признаками
+        def del_sign():
+            sign_name.grid_forget()
+            sign_name_entry.grid_forget()
+            sign_name_add.grid_forget()
+            sign_list.grid_forget()
+
+        # Удаляются все виджеты для работы с возможными значениями
+        def del_possible_values():
+            pass
+        
+        # Удаляются все виджеты для работы с признаками класса
+        def del_class_sign():
+            pass
+
+        # Удаляются все виджеты для работы с значениями для класса
+        def del_class_values():
+            pass
+
+        # Удаляются все виджеты для работы с проверкой полноты знаний
+        def del_check():
+            pass
+
+
+
+        # Создаются необходимые виджеты для работы с классом
+        def create_class_solid():
+            delete_btn()
+            normal_btn()
+            btn_class_soil['state'] = 'disabled'
+
+            global class_name, class_name_entry, class_name_add, class_list
+            class_name = tk.Label(fld_frame, text='Название класса', bg='#D9D9D9')
+            class_name.grid(row=0, column=0, sticky='w')
+            class_name_entry = tk.Entry(fld_frame)
+            class_name_entry.grid(row=1, column=0)
+            class_name_add = tk.Button(fld_frame, text='Добавить')
+            class_name_add.grid(row=1, column=1, padx=5)
+            class_list = tk.Label(fld_frame, text='Список классов', bg='#D9D9D9')
+            class_list.grid(row=2, column=0, sticky='w')
+
+        # Создаются необходимые виджеты для работы с признаками
+        def create_sign():
+            delete_btn()
+            normal_btn()
+            btn_sign['state'] = 'disabled'
+            
+            global sign_name, sign_name_entry, sign_name_add, sign_list
+            sign_name = tk.Label(fld_frame, text='Название признака', bg='#D9D9D9')
+            sign_name.grid(row=0, column=0, sticky='w')
+            sign_name_entry = tk.Entry(fld_frame)
+            sign_name_entry.grid(row=1, column=0)
+            sign_name_add = tk.Button(fld_frame, text='Добавить')
+            sign_name_add.grid(row=1, column=1, padx=5)
+            sign_list = tk.Label(fld_frame, text='Список признаков', bg='#D9D9D9')
+            sign_list.grid(row=2, column=0, sticky='w')
+        
+        # Создаются необходимые виджеты для работы с возможными значениями
+        def create_possible_values():
+            delete_btn()
+            normal_btn()
+            btn_possible_values['state'] = 'disabled'
+
+        # Создаются необходимые виджеты для работы с признаками класса
+        def create_class_sign():
+            delete_btn()
+            normal_btn()
+            btn_class_sign['state'] = 'disabled'
+
+        # Создаются необходимые виджеты для работы с значениями для класса
+        def create_class_values():
+            delete_btn()
+            normal_btn()
+            btn_class_values['state'] = 'disabled'
+
+        # Создаются необходимые виджеты для работы с проверкой полноты знаний
+        def create_check():
+            delete_btn()
+            normal_btn()
+            btn_check['state'] = 'disabled'
+
+
+        btn_class_soil = tk.Button(btn_frame, text='Класс почвы', command=create_class_solid)
         btn_class_soil.grid(row=0, sticky='we', pady=5)
-        btn_sign = tk.Button(btn_frame, text='Признаки')
+        btn_sign = tk.Button(btn_frame, text='Признаки', command=create_sign)
         btn_sign.grid(row=1, sticky='we', pady=5)
-        btn_possible_values = tk.Button(btn_frame, text='Возможные значения')
+        btn_possible_values = tk.Button(btn_frame, text='Возможные значения', command=create_possible_values)
         btn_possible_values.grid(row=2, sticky='we', pady=5)
-        btn_class_sign = tk.Button(btn_frame, text='Признаки класса')
+        btn_class_sign = tk.Button(btn_frame, text='Признаки класса', command=create_class_sign)
         btn_class_sign.grid(row=3, sticky='we', pady=5)
-        btn_class_values = tk.Button(btn_frame, text='Значения для класса')
+        btn_class_values = tk.Button(btn_frame, text='Значения для класса', command=create_class_values)
         btn_class_values.grid(row=4, sticky='we', pady=5)
-        btn_check = tk.Button(btn_frame, text='Проверка полноты \nзнаний')
+        btn_check = tk.Button(btn_frame, text='Проверка полноты \nзнаний', command=create_check)
         btn_check.grid(row=5, sticky='we', pady=5)
 
-        bt2 = tk.Button(fld_frame, text='bt2')
-        bt2.grid()
+        # При открытии окна кнопка "Класс почвы" уже нажата. Окно готово к работе с классами
+        create_class_solid()
 
-    # По нажатию кнопок в btn_frame будут отображаться данные в fld_frame
-    def create_field():
-        pass
+        # bt2 = tk.Button(fld_frame, text='bt2')
+        # bt2.grid()
+
+        
+
+    
 
 # Окно для решения задачи
 class SolveTheTask(tk.Toplevel):
